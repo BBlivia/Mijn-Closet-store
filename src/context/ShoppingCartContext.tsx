be89cfc12437a3d7d62be2e6fdbd2b0fee6d8761
Olivia Bloway <blowayol@msu.edu>
2022-03-04 08:33:21 -0500

    finished cart type

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -7,6 +7,8 @@ type ShoppingCartProviderProps = {
 type ShoppingCartContext ={
     getItemQuantity:(id: number) => number
     increaseCartQuantity: (id: number) => void
+    decreaseCartQuantity: (id: number)=> void
+    removeFromCart:(id: number) => void 
 }
 
 const ShoppingCartContext = createContext({})
