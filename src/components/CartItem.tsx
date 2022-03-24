520d4e668e923cd1795f12394495dc8db54cfdd7
Olivia Bloway <blowayol@msu.edu>
2022-03-24 11:53:11 -0400

    imported data to shopping cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import {  Stack } from "react-bootstrap";
+import {  Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import { StoreItem } from "./StoreItem"
 import storeItems from "../data/items.json"
@@ -15,19 +15,43 @@ export function CartItem({id, quantity}: CartItemProps){
     if( item == null) return null 
 
     return(
-        <Stack direction="horizontal" gap={2} className="d-flex align-items-center" >
-            <img src={item.imgUrl} style={{width: "125px", height:"75 px", objectFit: "cover"}} />
-            <div className="me-auto"> 
-                <div>
-                {item.name} {quantity > 1 && <span className="text-muted" style={{fontSize: '.65rem'}} >x{quantity}</span>}
-                </div>
-                <div className="text-muted" style={{fontSize:".75rem"}} >  {formatCurrency(item.price)}</div>
-                <div> {formatCurrency(item.price * quantity)}</div>
+        
+
+
+
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+        <img
+            src={item.imgUrl}
+            style={{ width: "125px", height: "75px", objectFit: "cover" }}
+        />
+        <div className="me-auto">
+            <div>
+            {item.name}{" "}
+            {quantity > 1 && (
+                <span className="text-muted" style={{ fontSize: ".65rem" }}>
+                x{quantity}
+                </span>
+            )}
             </div>
-            
-          
+            <div className="text-muted" style={{ fontSize: ".75rem" }}>
+            {formatCurrency(item.price)}
+            </div>
+        </div>
+        <div> {formatCurrency(item.price * quantity)}</div>
+        <Button
+            variant="default"
+            size="sm"
+            onClick={() => removeFromCart(item.id)}
+        >  <svg width="15" height="14" fill="none" stroke="#000" className="trash-svg"><path d="M11,13H4a1,1,0,0,1-1-1V3h9v9a1,1,0,0,1-1,1Z"></path><path d="M6,10V6"></path><path d="M9,10V6"></path><path d="M1,3H14"></path><path d="M9,1H6a1,1,0,0,0-.71.29A1,1,0,0,0,5,2V3h5V2a1,1,0,0,0-.29-.71A1,1,0,0,0,9,1Z"></path></svg>
            
-         </Stack>
+        </Button>
+        </Stack>
+
+
+
+                 
+           
+            
     )
 
 }
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { NavItem, Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utility/formatCurreny";
 import { CartItem } from "./CartItem"
+import storeItems from "../data/items.json"
 
 type ShoppingCartProps = {
     isOpen : boolean
@@ -8,14 +10,26 @@ type ShoppingCartProps = {
 
 export function ShoppingCart({isOpen}: ShoppingCartProps){
     const {closeCart, cartItems} = useShoppingCart()
-    return <Offcanvas show={isOpen} onHide={closeCart} placement="end" >
+    return (<Offcanvas show={isOpen} onHide={closeCart} placement="end" >
         <Offcanvas.Header closeButton> <Offcanvas.Title>Cart</Offcanvas.Title></Offcanvas.Header>
         <Offcanvas.Body>
             <Stack gap={3} >
                 {cartItems.map(item => ( 
                      <CartItem key={item.id} {...item} />
                  )) }
+
+
+                 <div className="ms-auto fw-bold fs-5">
+                    Total{" "} 
+                    {formatCurrency(
+                        cartItems.reduce((total, cartItem) => {
+                        const item = storeItems.find(i => i.id === cartItem.id)
+                        return total + (item?.price || 0) * cartItem.quantity
+                    }, 0)
+                    )}
+                 </div>
             </Stack>
         </Offcanvas.Body>
     </Offcanvas>
+    )
 }
